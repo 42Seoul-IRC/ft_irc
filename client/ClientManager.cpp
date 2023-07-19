e1508c963aff3c8623be58d753b47a27f4a212b0
Taekwon Yi <tyi@c4r3s3.42seoul.kr>
2023-07-19 10:21:11 +0900

    Feat : printAllClients function add

--- client/ClientManager.cpp
+++ client/ClientManager.cpp
@@ -14,3 +14,28 @@ Client	*ClientManager::getClientBySocket(int socket)
 		return (NULL);
 	return (socket_clients_.find(socket)->second);
 }
+
+void ClientManager::printAllClients()
+{
+	std::map<int, Client*>::iterator it;
+
+	int i = 0;
+	for (it = socket_clients_.begin(); it != socket_clients_.end(); it++)
+	{
+		std::cout << "--------------------------" << std::endl;
+		std::cout << "client " << i << std::endl;
+
+		std::cout << "--------------------------" << std::endl;
+		std::cout << "socket: " << it->first << std::endl;
+		std::cout << "nickname: " << it->second->getNickName() << std::endl;
+		std::cout << "username: " << it->second->getUserName() << std::endl;
+		std::cout << "hostname: " << it->second->getHostName() << std::endl;
+		std::cout << "channels: " << std::endl;
+		std::set<std::string>::iterator it2;
+		for (it2 = it->second->getChannels().begin(); it2 != it->second->getChannels().end(); it2++)
+			std::cout << *it2 << std::endl;
+		std::cout << "--------------------------" << std::endl;
+		std::cout << std::endl;
+		i++;
+	}
+}
