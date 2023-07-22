eaa7d4e6a5b20744777cc5b7fe2cc38fa7c7d2e4
taekwonlee <teemo20001005@gmail.com>
2023-07-22 20:02:12 +0900

    Feat : message  synchoronize with ircpd

--- packet/macro.h
+++ packet/macro.h
@@ -55,7 +55,7 @@
 #define ERR_BADCHANNELKEY "475"
 #define ERR_BADCHANNELKEY_MSG "Bad channel key"
 #define ERR_INVITEONLYCHAN "473"
-#define ERR_INVITEONLYCHAN_MSG "Invite only channel"
+#define ERR_INVITEONLYCHAN_MSG "Cannot join channel (invite only)"
 #define ERR_CHANNELISFULL "471"
 #define ERR_CHANNELISFULL_MSG "Channel is full"
 #define ERR_NOTREGISTERED "451"
